226887a80a40e8d4fdf8c02fd654dc2e445463c1
JohnVonNeumann <louiswillcock@gmail.com>
2016-11-11 06:26:38 +0000

    added content presence + lengrth validation to respective models, linked user and micropost models via has_many and belongs_to for db purposes and pushed to heroku

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,2 +1,8 @@
 class Micropost < ApplicationRecord
+    belongs_to :user
+    #indicates that each micropost onbject belongs to one specific user
+    validates :content, length: { maximum: 140 },
+                        presence: true
+    #validates the content input from our page so that if the string length (our chosen field param)
+    #is longer than 140 chars, it throws an error
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+    has_many :microposts
+    #aligns the user object with it's counterpart micropost, this 
+    #dictates that one user can create/own many microposts
+    validates FILL_IN, presence: true #relates to name field, replace fill_in with the right code
+    validates FILL_IN, presence: true #relates to content field, replace fill_in witht he right code 
 end
